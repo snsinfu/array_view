71a30360e968a9fa7eb64573b8520c712f0c9f5b
snsinfu <snsinfu@gmail.com>
2018-01-17 04:01:22 +0900

    Initial commit

---
+++ tests/test_smoketest.cc
@@ -0,0 +1,8 @@
+#include <array_view.hpp>
+#include <array_view.hpp> // Test include guard
+#include <catch.hpp>
+
+TEST_CASE("array_view<int> is defined")
+{
+    CHECK(sizeof(snsinfu::ext::array_view<int>) > 0);
+}
