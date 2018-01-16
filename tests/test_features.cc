9166765e7c771294e10e22900a5b13b174664279
snsinfu <snsinfu@gmail.com>
2018-01-17 05:26:37 +0900

    Reorganize tests

--- tests/test_features.cc
+++ tests/test_features.cc
@@ -9,60 +9,15 @@ namespace ext = snsinfu::ext;
 TEST_CASE("array_view can access vector elements")
 {
     std::vector<int> vector = {0, 1, 2, 3};
-    ext::array_view<int> const view = ext::view(vector);
 
-    SECTION("reading elements via indices")
+    SECTION("smoke test")
     {
-        CHECK(view[0] == 0);
-        CHECK(view[2] == 2);
+        ext::array_view<int> view = ext::view(vector);
     }
 
-    SECTION("modifying elements via indexing")
+    SECTION("won't throw")
     {
-        view[0] = 100;
-        CHECK(vector[0] == 100);
-    }
-
-    SECTION("accessing front and back elements")
-    {
-        CHECK(view.front() == 0);
-        CHECK(view.back() == 3);
-    }
-
-    SECTION("getting raw pointer")
-    {
-        CHECK(view.data() == vector.data());
-    }
-
-    SECTION("getting the number of elements")
-    {
-        CHECK(view.size() == vector.size());
-    }
-
-    SECTION("iterating with range interface")
-    {
-        for (int& elm : view) {
-            elm += 10;
-        }
-        CHECK(vector == std::vector<int>{10, 11, 12, 13});
-    }
-
-    SECTION("indexing with bounds-check")
-    {
-        CHECK_NOTHROW(view.at(0));
-        CHECK_THROWS_AS(view.at(view.size()), std::out_of_range);
-    }
-
-    SECTION("reverse iterator")
-    {
-        auto beg = view.rbegin();
-        auto end = view.rend();
-
-        auto it = beg;
-        CHECK(*it == vector.back());
-
-        it += view.end() - view.begin();
-        CHECK(it == end);
+        CHECK(noexcept(ext::view(vector)));
     }
 }
 
@@ -119,6 +74,66 @@ TEST_CASE("array_view is default constructible")
     ext::array_view<int> view;
     CHECK(view.data() == nullptr);
     CHECK(view.size() == 0);
+}
+
+TEST_CASE("")
+{
+    std::vector<int> vector = {0, 1, 2, 3};
+    ext::array_view<int> view = ext::view(vector);
+
+    SECTION("reading elements via indices")
+    {
+        CHECK(view[0] == 0);
+        CHECK(view[2] == 2);
+    }
+
+    SECTION("modifying elements via indexing")
+    {
+        view[0] = 100;
+        CHECK(vector[0] == 100);
+    }
+
+    SECTION("accessing front and back elements")
+    {
+        CHECK(view.front() == 0);
+        CHECK(view.back() == 3);
+    }
+
+    SECTION("getting raw pointer")
+    {
+        CHECK(view.data() == vector.data());
+    }
+
+    SECTION("getting the number of elements")
+    {
+        CHECK(view.size() == vector.size());
+    }
+
+    SECTION("iterating with range interface")
+    {
+        for (int& elm : view) {
+            elm += 10;
+        }
+        CHECK(vector == std::vector<int>{10, 11, 12, 13});
+    }
+
+    SECTION("indexing with bounds-check")
+    {
+        CHECK_NOTHROW(view.at(0));
+        CHECK_THROWS_AS(view.at(view.size()), std::out_of_range);
+    }
+
+    SECTION("reverse iterator")
+    {
+        auto beg = view.rbegin();
+        auto end = view.rend();
+
+        auto it = beg;
+        CHECK(*it == vector.back());
+
+        it += view.end() - view.begin();
+        CHECK(it == end);
+    }
 }
 
 TEST_CASE("array_view supports constexpr for literal strings")
